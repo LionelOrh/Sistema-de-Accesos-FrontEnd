9b1bb02c1e3f514b6252ea89c8f708b39e5703ac
AstridYovera <ayoveratinoco@gmail.com>
2024-11-18 11:17:05 -0500

    feat: cambios en el menú

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from '../security/token.service';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppMaterialModule } from '../app.material.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from '../menu/menu.component';
-import { Opcion } from '../security/opcion';
+
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [AppMaterialModule, RouterLink,FormsModule, CommonModule, MenuComponent],
+  imports: [RouterOutlet, RouterLink, AppMaterialModule, FormsModule, CommonModule, MenuComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
@@ -18,47 +18,17 @@ export class IndexComponent implements OnInit {
 
   isLogged = false;
   nombreUsuario = "";
-  opcRegistroEntradaSalida: Opcion[] = [];
-  opcRegistroVisita: Opcion[] = [];
-  opcConsultaReporte: Opcion[] = [];
-  opcAccesoProveedor: Opcion[] = [];
 
-  constructor(private tokenService: TokenService, private router: Router) {  
-    console.log("MenuComponent >>> constructor >>> " + this.tokenService.getToken());
-  }
+  constructor(private tokenService: TokenService) { }
 
   ngOnInit() {
     if (this.tokenService.getToken()) {
       this.isLogged = true;
-      this.nombreUsuario = this.tokenService.getUserNameComplete() || '{}';
+      this.nombreUsuario = this.tokenService.getUserNameComplete()|| '{}';
     } else {
       this.isLogged = false;
       this.nombreUsuario = '';
-      this.router.navigate(['/login']);
-    }
-    console.log("MenuComponent >>> ngOnInit >>> ");
-
-    this.opcRegistroEntradaSalida = this.tokenService.getOpciones().filter(x => x.tipo === 1);
-    this.opcRegistroVisita = this.tokenService.getOpciones().filter(x => x.tipo === 2);
-    this.opcConsultaReporte= this.tokenService.getOpciones().filter(x => x.tipo === 3);
-    this.opcAccesoProveedor = this.tokenService.getOpciones().filter(x => x.tipo === 4);
-
-    console.log("MenuComponent >>> ngOnInit >>> " + this.tokenService.getToken());
-    this.isLogged = !!this.tokenService.getToken(); 
-    console.log(`MenuComponent >>> this.isLogged = ${this.isLogged} >>> `);
-  }
-  onLogOut(): void {
-    this.tokenService.logOut();
-    this.router.navigate(['/login']);
-  }
-
-  obtenerRol(): string {
-    if (this.opcConsultaReporte.length > 0) {
-      return 'Supervisor';
-    } else if (this.opcRegistroEntradaSalida.length > 0) {
-      return 'Personal de Seguridad';
-    } else {
-      return 'Rol Desconocido';
     }
   }
+
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Opcion } from '../security/opcion';
 import { TokenService } from '../security/token.service';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppMaterialModule } from '../app.material.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Usuario } from '../models/usuario.model';
+import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-menu',
@@ -21,28 +24,103 @@ export class MenuComponent implements OnInit {
   opcRegistroInvitado: Opcion[] = [];
   opcConsultaReporte: Opcion[] = [];
   opcAccesoProveedor: Opcion[] = [];
+  nombreUsuario = "";
+  fotoUsuario = "";
+  idUsuario = 0;  
+  
+  get saludo(): string {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+        return '¡Buenos días!';
+    } else if (hour < 18) {
+        return '¡Buenas tardes!';
+    } else {
+        return '¡Buenas noches!';
+    }
+}
 
-  constructor(private tokenService: TokenService, private router: Router) {  
+  get horaActual(): string {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');  
+    const minutes = now.getMinutes().toString().padStart(2, '0'); 
+    return `${hours}:${minutes}`;
+  }
+
+  // Getter para la fecha actual en formato: Día de la semana, Día de mes, Año
+  get fechaActual(): string {
+    const now = new Date();
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short',
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    };
+    return now.toLocaleDateString('es-ES', options); // Formato español
+  }
+  constructor(private tokenService: TokenService, private router: Router, private http: HttpClient) {
     console.log("MenuComponent >>> constructor >>> " + this.tokenService.getToken());
   }
 
   ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+      this.nombreUsuario = this.tokenService.getUserNameComplete();
+      this.idUsuario = this.tokenService.getUserId();
+      this.getUserPhoto(this.idUsuario);
+
+      this.opcRegistroEntradaSalida = this.tokenService.getOpciones().filter(x => x.tipo === 1);
+      this.opcRegistroInvitado = this.tokenService.getOpciones().filter(x => x.tipo === 2);
+      this.opcConsultaReporte = this.tokenService.getOpciones().filter(x => x.tipo === 3);
+      this.opcAccesoProveedor = this.tokenService.getOpciones().filter(x => x.tipo === 4);
+    } else {
+      this.isLogged = false;
+      this.nombreUsuario = '';
+      this.router.navigate(['/login']);
+    }
     console.log("MenuComponent >>> ngOnInit >>> ");
-
-    this.opcRegistroEntradaSalida = this.tokenService.getOpciones().filter(x => x.tipo === 1);
-    this.opcRegistroInvitado = this.tokenService.getOpciones().filter(x => x.tipo === 2);
-    this.opcConsultaReporte= this.tokenService.getOpciones().filter(x => x.tipo === 3);
-    this.opcAccesoProveedor = this.tokenService.getOpciones().filter(x => x.tipo === 4);
-
-    console.log("MenuComponent >>> ngOnInit >>> " + this.tokenService.getToken());
-    this.isLogged = !!this.tokenService.getToken(); 
-    console.log(`MenuComponent >>> this.isLogged = ${this.isLogged} >>> `);
   }
 
+  getUserPhoto(idUsuario: number) {
+    // Ahora solo asignamos la URL del backend para la foto
+    this.fotoUsuario = `http://localhost:8090/url/usuario/${idUsuario}`;
+  }
+  
   onLogOut(): void {
-    this.tokenService.logOut();
-    this.router.navigate(['/login']);
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¿Quieres cerrar sesión?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, cerrar sesión',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Mostrar el mensaje de carga
+        Swal.fire({
+          title: 'Cerrando sesión...',
+          text: 'Por favor, espera...',
+          allowOutsideClick: false,
+          didOpen: () => {
+            Swal.showLoading();
+          }
+        });
+  
+        // Simulamos un retraso para mostrar la carga (en un caso real sería el tiempo de procesamiento)
+        setTimeout(() => {
+          // Llamar al método de cierre de sesión
+          this.tokenService.logOut();
+          // Redirigir al login
+          this.router.navigate(['/login']);
+          
+          // Cerrar el mensaje de carga
+          Swal.close();
+        }, 2000); // 2 segundos de retraso antes de redirigir
+      }
+    });
   }
+  
 
   obtenerRol(): string {
     if (this.opcConsultaReporte.length > 0) {
