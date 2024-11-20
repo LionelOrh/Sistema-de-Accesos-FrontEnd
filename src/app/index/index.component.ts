c195d96521b2e44ddb854270c13954aa8c976726
AstridYovera <ayoveratinoco@gmail.com>
2024-11-19 20:33:57 -0500

    feat: arreglos en las dimensiones de las páginas y contenido en el index

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,34 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TokenService } from '../security/token.service';
-import { RouterLink, RouterOutlet } from '@angular/router';
 import { AppMaterialModule } from '../app.material.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MenuComponent } from '../menu/menu.component';
 
-
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, AppMaterialModule, FormsModule, CommonModule, MenuComponent],
+  imports: [AppMaterialModule, FormsModule, CommonModule, MenuComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, OnDestroy {
 
   isLogged = false;
   nombreUsuario = "";
+  currentIndex = 0;
+  intervalId: any;
 
   constructor(private tokenService: TokenService) { }
 
   ngOnInit() {
     if (this.tokenService.getToken()) {
       this.isLogged = true;
-      this.nombreUsuario = this.tokenService.getUserNameComplete()|| '{}';
+      this.nombreUsuario = this.tokenService.getUserNameComplete() || '{}';
     } else {
       this.isLogged = false;
       this.nombreUsuario = '';
     }
+
+    this.intervalId = setInterval(() => {
+      this.showNext();
+    }, 5000);
   }
 
+  showPrev(): void {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : 2; // Volver al último comunicado
+  }
+
+  showNext(): void {
+    this.currentIndex = (this.currentIndex < 2) ? this.currentIndex + 1 : 0; // Volver al primer comunicado
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
