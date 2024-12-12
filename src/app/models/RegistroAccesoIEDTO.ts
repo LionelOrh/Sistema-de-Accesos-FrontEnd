4a879b7fe46c02b9cde946a385b6411442b7afba
AstridYovera <ayoveratinoco@gmail.com>
2024-12-12 00:14:20 -0500

    feat:optimizacion en consulta, pero falta ver algunas cosas

---
+++ src/app/models/RegistroAccesoIEDTO.ts
@@ -0,0 +1,9 @@
+export class preRegistroConsultaDTO {
+    codigoIE?: string;
+    nombresIE?: string;
+    apellidosIE?: string;
+    numDocIE?: string;
+    tipoAcceso?: string;
+    fechaAcceso?: string;
+    horaAcceso?: string;
+}
